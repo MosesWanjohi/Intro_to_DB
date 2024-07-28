a72a4a52149569020c32816cbcb0ca912a738951
Moses Wanjohi <wanjohimuturi.tech@gmail.com>
2024-07-28 08:33:06 -0700

    script that inserts a single row in the table customer

---
+++ task_5.sql
@@ -0,0 +1,7 @@
+#script that inserts a single row in the table customer (database alx_book_store)
+
+USE alx_book_store;
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
